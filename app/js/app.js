ed9e5318cf76de9deb2cbb08df298b36bbe3b3b5
Dariia <dashasalmiiarova@icloud.com>
2020-04-14 15:13:18 +0200

    with menu and slogan began

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+$('.slogan-carousel').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 600,
+  cssEase: 'linear',
+  arrows: true,
+  centerMode: true,
+  centerPadding: '0px'
+})
