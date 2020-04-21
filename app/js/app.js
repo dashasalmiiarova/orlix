fe76d1fe78a32f8d706b399a3ab1222e665528d9
Dariia <dashasalmiiarova@icloud.com>
2020-04-21 13:01:29 +0200

    started galery slider

--- app/js/app.js
+++ app/js/app.js
@@ -12,4 +12,15 @@ $('.slogan-carousel').slick({
   nextArrow: '<img src="img/slider-arrow-right.svg" class="slider-arrows slider-arrow-right" alt=""></img>'
 })
 
-   
+$('.galeria-carousel').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  speed: 600,
+  arrows: true,
+  centerMode: true,
+  focusOnSelect: true,
+  centerPadding: '0px',
+  prevArrow: '<img src="img/slider-galeria-left.svg" class="galeria-slider-arrows galeria-arrow-left" alt="">',
+  nextArrow: '<img src="img/slider-galeria-right.svg" class="galeria-slider-arrows galeria-arrow-right" alt=""></img>'
+})
